02b9d7fd750b853e5f8856f3bfe81af36f367bd7
Freddy Lemus <sumel124@gmail.com>
2019-07-09 05:44:11 -0500

    Create a statefull class App

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [
+        {
+          nombre: "Rodolfo",
+          correo: "rodolfo@platzi.com",
+          enlace: "rodolfo.com"
+        },
+        {
+          nombre: "Rodolfito",
+          correo: "rodolfo@platzi.com",
+          enlace: ""
+        },
+        {
+          nombre: "Platzi",
+          correo: "info@platzi.com",
+          enlace: "platzi.com"
+        }
+      ]
+    };
+  }
+
+  ponerFilas = () =>
+    this.state.usuarios.map(usuario => (
+      <tr>
+        <td>{usuario.nombre}</td>
+        <td>{usuario.correo}</td>
+        <td>{usuario.enlace}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
